990bbed0972dda178fe4b3288d450204684d355a
Aiden Taylor <aiden.taylor@protonmail.com>
2023-08-15 08:01:51 -0600

    Removed functions that have been moved to ``argparse.rs``. Modified to now use the two "systems" of use.

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,24 @@
 use std::env;
 
-fn argparse (arguments: Vec<String>) -> Vec<u32> {
-  let mut numbers: Vec<u32> = Vec::new();
-  let mut buffer: u32;  
-  let arglength: usize = arguments.len();  
- 
-  // Rudimentary --help system. To be updated.
-  if arguments[1].eq("--help"){
-    println!("HELP SCREEN FOR QSCALETOOL");
-    println!("Usage:\n./qscaletool <Texture Width> <Texture Height> <Brush Width> <Brush Height>");
-    panic!();
-  }
-
-  if arglength > 5 {
-    println!("Warning! All arguments past the fourth ({}) will be ignored!",arguments[4]);
-  }
-
-  // To be updated to include "presets" of sorts.
-  if arglength < 4 {
-    panic!("Error! Not enough arguments have been passed into program! Use --help for more info!");
-  }
-
-  // Writing arguments to numbers.
-  for i in 1..arglength {
-
-    buffer = arguments[i].parse::<u32>().unwrap();
-    numbers.push(buffer)
-  }
-
-  numbers
-}
+mod argparse;
+use crate::argparse::argparse;
 
 fn main() { 
- 
+  // First we collect arguments, and length of the arguments. 
   let args: Vec<String> = env::args().collect();
-  let numbers: Vec<u32> = argparse(args);
-  println!("Pre-Calculation Stats:");
-  println!("Texture Width (X-Size):\t{}\tTexture Height (Y-Size):\t{}",numbers[0],numbers[1]);
-  println!("Brush Width (X-Size):\t{}\tBrush Height (Y-Size):\t\t{}",numbers[2],numbers[3]);
+  let arguments: Vec<u32> = argparse(args);
+  let arglength: usize = arguments.len();
 
-  let xscale: f32 = numbers[2] as f32 / numbers[0] as f32;
-  let yscale: f32 = numbers[3] as f32 / numbers[1] as f32;
-  println!("X Scale:\t{}\tY Scale:\t{}",xscale,yscale)
+  // Then we see which mode we're using.
+  if arglength == 2 { // We're doing "Simple" mode.
+    println!("Texture size: {}",arguments[0]);
+    println!("Brush size:   {}",arguments[1]);
+    println!("Scale:        {}",arguments[1] as f32 / arguments[0] as f32);
+  }
+
+  if arglength == 4 { // We're doing "Default" mode.
+    println!("Texture width: {}\tTexture height: {}",arguments[0],arguments[1]);
+    println!("Brush width:   {}\tBrush height:   {}",arguments[2],arguments[3]);
+    println!("X-Scale:       {}\tY-Scale:        {}",arguments[2] as f32 / arguments[0] as f32, arguments[3] as f32 / arguments[1] as f32);
+  }
 }
